d8bf91d48a4461cbe7560df2d35053dd7fe69a87
stone-w4tch3r <bizi1298@gmail.com>
2022-09-22 16:18:01 +0500

    internalsvisibleto

--- RolePlayer.Model.Tests/Core/Infrastructure/OnChangeActingWrapperTests.cs
+++ RolePlayer.Model.Tests/Core/Infrastructure/OnChangeActingWrapperTests.cs
@@ -1,6 +1,13 @@
+using RolePlayer.Model.API.Interfaces;
+using RolePlayer.Model.Core.Infrastructure.Classes;
+
 namespace RolePlayer.Model.Tests.Core.Infrastructure;
 
 public class OnChangeActingWrapperTests
 {
-    
+    [Test]
+    public void Test()
+    {
+        var wrapper = new OnChangeActingWrapper<IDatabase, FileInfo>();
+    }
 }
