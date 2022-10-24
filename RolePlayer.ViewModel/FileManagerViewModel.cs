2668cc07e9c37c1297d3e702fd19f241c9e5697d
stone-w4tch3r <bizi1298@gmail.com>
2022-10-24 12:19:24 +0500

    Command, BaseVm ...

--- RolePlayer.ViewModel/FileManagerViewModel.cs
+++ RolePlayer.ViewModel/FileManagerViewModel.cs
@@ -1,9 +1,25 @@
 ﻿using System.ComponentModel;
+using System.Windows.Input;
 using RolePlayer.Model.API.Interfaces;
 
 namespace RolePlayer.ViewModel;
 
-public class FileManagerViewModel
+public class FileManagerViewModel : BaseViewModel
 {
-    public IEnumerable<IStory> Stories { get; set; }
+    private IEnumerable<IStory> _stories;
+    public IEnumerable<IStory> Stories
+    {
+        get => _stories;
+        set => SetField(ref _stories, value);
+    }
+    public ICommand AddStoriesCommand { get; }
+    internal FileManagerViewModel(IEnumerable<IStory> stories)
+    {
+        _stories = stories;
+        AddStoriesCommand = new Command(AddStories);
+    }
+    private void AddStories()
+    {
+        
+    }
 }
