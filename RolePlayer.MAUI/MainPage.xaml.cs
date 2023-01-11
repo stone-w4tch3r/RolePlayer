e3552277a5fc02437fabf7038c67edb90368b297
dbizimov <d.bizimov@e-promo.ru>
2023-01-11 11:26:31 +0500

    maui mainpage cleaned

--- RolePlayer.MAUI/MainPage.xaml.cs
+++ RolePlayer.MAUI/MainPage.xaml.cs
@@ -2,30 +2,11 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = int.MaxValue - 5;
+	public MainPage() => InitializeComponent();
 
-	public MainPage()
+	private void OnLoadFileCLicked(object sender, EventArgs e)
 	{
-		InitializeComponent();
-	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
 		
-		if (count % 2 == 0)
-		{
-			Image.Source = ImageSource.FromFile("index.jpg");
-		}
-		else
-		{
-			Image.Source = ImageSource.FromFile("dotnet_bot.png");
-		}
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
