d27f9ab1858d006766fbdead303ba1daed0ac4a1
d.bizimov <d.bizimov@e-promo.ru>
2023-03-11 16:10:22 +0500

    progressbar & activityindicator

--- RolePlayer.MAUI/MainPage.xaml.cs
+++ RolePlayer.MAUI/MainPage.xaml.cs
@@ -2,17 +2,56 @@
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    private readonly ActivityIndicator _activityIndicator = new() { IsRunning = true, Color = Colors.SeaGreen };
+    private readonly Label _label = new() { Text = "Процесс 1" };
+    private readonly Label _label2 = new() { Text = "Процесс 2" };
+    private readonly ProgressBar _progressBar = new() { ProgressColor = Colors.SeaGreen };
 
-	private void OnLoadFileCLicked(object sender, EventArgs e)
-	{
-		
+    public MainPage()
+    {
+        Content = new StackLayout
+        {
+            Padding = 20,
+            Children = { _label, _label2, _progressBar, _activityIndicator }
+        };
+        // InitializeComponent();
+    }
 
-		// SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    protected override async void OnAppearing()
+    {
+        await RunProgress();
+        await RunActivityIndicator();
+        base.OnAppearing();
+
+        async Task RunActivityIndicator()
+        {
+            var count = 0;
+            while (count != 100)
+            {
+                _label2.Text = $"Состояние процесса: {count} %";
+                await Task.Delay(2000);
+                count += 10;
+            }
+
+            _label2.Text = "Процесс закончен";
+            _activityIndicator.IsRunning = false;
+        }
+
+        async Task RunProgress()
+        {
+            while (_progressBar.Progress < 0.9999)
+            {
+                _progressBar.Progress += 0.0001;
+                _label.Text = $"Состояние процесса: {Math.Round(_progressBar.Progress, 2) * 100} %";
+                await Task.Delay(1);
+            }
+
+            _label.Text = "Процесс закончен";
+        }
+    }
+
+    private void OnLoadFileCLicked(object sender, EventArgs e)
+    {
+        // SemanticScreenReader.Announce(CounterBtn.Text);
+    }
 }
-
-
