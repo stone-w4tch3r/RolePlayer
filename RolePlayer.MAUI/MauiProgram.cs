f9af4191cd52607d8ae47b60d0c18cf01a162057
stone-w4thc3r <no@e.mail>
2023-04-02 11:49:20 +0500

    MauiIcons used

--- RolePlayer.MAUI/MauiProgram.cs
+++ RolePlayer.MAUI/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using MauiIcons.Fluent;
+using Microsoft.Extensions.Logging;
 using RolePlayer.MAUI.Core.FunctionalExtensions;
 
 namespace RolePlayer.MAUI;
@@ -13,6 +14,7 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             })
+            .UseFluentMauiIcons()
 #if DEBUG
             .Map(x =>
             {
