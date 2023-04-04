8703468afe2d193ad984c36e4c56c0cec8c5e6ab
stone-w4thc3r <no@e.mail>
2023-04-04 22:12:34 +0500

    avalonia + restructuring

---
+++ RolePlayer.Avalonia/RolePlayer.Avalonia/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace RolePlayer.Avalonia.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ RolePlayer.Avalonia/RolePlayer.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace RolePlayer.Avalonia.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
